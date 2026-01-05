c33bfb6e22f5be2e9a8c19ccc5ae6304f6f38e07
Palanki Shashank Srinivas <shashankpalanki18@gmail.com>
2026-01-05 14:24:27 +0530

    Problem of hanoi towers

--- hanoi.c
+++ hanoi.c
@@ -1,1 +1,29 @@
-
+#include<stdio.h>
+void hanoi(int n,char s,char i,char d)
+{
+	
+	if(n==1)
+	{
+		printf("move disc %d from %c to %c\n",1,s,d);
+	    return;
+	}
+	hanoi(n-1,s,d,i);
+	printf("Move disc %d from %c to %c\n",n,s,d);
+	hanoi(n-1,i,s,d);
+	
+	
+		
+	
+}
+int main()
+{
+	printf("Enter the the number\n");
+	int n;
+	char a,b,c;
+	a= 's';
+	b= 'i';
+	c= 'd';
+	
+	scanf("%d",&n);
+	hanoi(n,a,b,c);
+}
